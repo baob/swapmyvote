7e6439f0640b1758f00157d0a684ec125ced97c9
baob <coder@onesandthrees.com>
2019-11-08 20:40:28 +0000

    make matching work for cities
    This is a combination of 7 commits.
    
    failing test
    birminghm test passing
    liverpool passing
    manchester and sheffield
    brighton and plymouth
    southampton and (some city) Central
    ealing and lewisham

--- app/models/constituency.rb
+++ app/models/constituency.rb
@@ -1,7 +1,25 @@
 class Constituency < ApplicationRecord
   has_many :polls
 
+  SUBDIVIDED_CITIES = %w[
+    Birmingham
+    Liverpool
+    Sheffield
+    Manchester
+    Plymouth
+    Brighton
+    Southampton
+    Ealing
+    Lewisham
+  ]
+
   def name_mapped_to_ons_name
-    name.strip
+    name_first_word = name.strip.split(" ").first
+    return name if name == name_first_word
+    return name if name == "#{name_first_word} Central"
+
+    return name.gsub(name_first_word, name_first_word + ",") if SUBDIVIDED_CITIES.include?(name_first_word)
+
+    name
   end
 end
--- spec/models/constituency_spec.rb
+++ spec/models/constituency_spec.rb
@@ -6,4 +6,29 @@ RSpec.describe Constituency, type: :model do
   describe "#polls" do
     specify { expect {subject.polls}.not_to raise_error }
   end
+
+  specify { expect(subject).to respond_to(:name) }
+  specify { expect(subject).to respond_to(:name_mapped_to_ons_name) }
+
+  examples = {
+    "Birmingham Ladywood" => "Birmingham, Ladywood",
+    "Liverpool Hallam" => "Liverpool, Hallam",
+    "Manchester Gorton" => "Manchester, Gorton",
+    "Manchester Central" => "Manchester Central",
+    "Sheffield Brightside and Hillsborough" => "Sheffield, Brightside and Hillsborough",
+    "Brighton Kemptown" => "Brighton, Kemptown",
+    "Plymouth Moorview" => "Plymouth, Moorview",
+    "Southampton Test" => "Southampton, Test",
+    "Ealing Southall" => "Ealing, Southall",
+    "Lewisham Deptford" => "Lewisham, Deptford"
+}
+
+
+  describe '#name_mapped_to_ons_name' do
+    examples.each do |(our_name, ons_name)|
+      it "when given #{our_name}, returns #{ons_name}" do
+        expect(described_class.new(name: our_name).name_mapped_to_ons_name).to eql(ons_name)
+      end
+    end
+  end
 end
