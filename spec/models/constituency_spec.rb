b17d6728b78a3d638c8d4b7b5cfdf4c382fa3e94
baob <coder@onesandthrees.com>
2019-11-09 12:07:24 +0000

    add #normalised_name to constituency

--- spec/models/constituency_spec.rb
+++ spec/models/constituency_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Constituency, type: :model do
   specify { expect(subject).to respond_to(:name) }
   specify { expect(subject).to respond_to(:name_mapped_to_ons_name) }
 
-  examples = {
+  ons_examples = {
     "Birmingham Ladywood" => "Birmingham, Ladywood",
     "Liverpool Hallam" => "Liverpool, Hallam",
     "Manchester Gorton" => "Manchester, Gorton",
@@ -26,13 +26,37 @@ RSpec.describe Constituency, type: :model do
     "Acton and Ealing Village" => "Acton and Ealing, Village",
     "Lewisham Deptford" => "Lewisham, Deptford",
     "Lewisham East" => "Lewisham East"
-}
+  }
 
+  normalised_examples = {
+    "Birmingham Ladywood" => "Birmingham Ladywood",
+    "Liverpool Hallam" => "Liverpool Hallam",
+    "Manchester Gorton" => "Manchester Gorton",
+    "Manchester Central" => "Manchester Central",
+    "Sheffield Central" => "Sheffield Central",
+    "Sheffield South East" => "Sheffield South East",
+    "Brighton Kemptown" => "Brighton Kemptown",
+    "Plymouth Moorview" => "Plymouth Moorview",
+    "Southampton Test" => "Southampton Test",
+    "Ealing Southall" => "Ealing Southall",
+    "Ealing North" => "Ealing North",
+    "Acton and Ealing Village" => "Acton|Ealing Village",
+    "Lewisham Deptford" => "Lewisham Deptford",
+    "Lewisham East" => "Lewisham East"
+  }
 
   describe '#name_mapped_to_ons_name' do
-    examples.each do |(our_name, ons_name)|
+    ons_examples.each do |(our_name, ons_name)|
       it "when given #{our_name}, returns #{ons_name}" do
         expect(described_class.new(name: our_name).name_mapped_to_ons_name).to eql(ons_name)
+      end
+    end
+  end
+
+  describe '#normalised_name' do
+    normalised_examples.each do |(our_name, ons_name)|
+      it "when given #{our_name}, returns #{ons_name}" do
+        expect(described_class.new(name: our_name).normalised_name).to eql(ons_name)
       end
     end
   end
