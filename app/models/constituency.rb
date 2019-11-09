74d4ad700b8ec65acceadd6069f08ca408d2d57c
baob <coder@onesandthrees.com>
2019-11-09 18:48:30 +0000

    remove map to ons name from constituency

--- app/models/constituency.rb
+++ app/models/constituency.rb
@@ -24,12 +24,6 @@ class Constituency < ApplicationRecord
     "South"
   ]
 
-  def name_mapped_to_ons_name
-    name_split_by_and = name.match(/^(.*) and (.*)$/)
-    return single_name_mapped_to_ons_name(name) if name_split_by_and.nil?
-    name_split_by_and.captures.map{ |n| single_name_mapped_to_ons_name(n) }.join(" and ")
-  end
-
   def normalised_name
     name_split_by_and = name.match(/^(.*) and (.*)$/)
     return single_name_mapped_to_normalised_name(name) if name_split_by_and.nil?
@@ -38,19 +32,6 @@ class Constituency < ApplicationRecord
   end
 
   private
-
-  def single_name_mapped_to_ons_name(single_name)
-    name_first_word = single_name.strip.split(" ").first
-    compass_part = COMPASS_PARTS.detect{ |cp| "#{name_first_word} #{cp}" == single_name }
-    return single_name unless compass_part.nil?
-    return single_name if single_name == name_first_word
-    return single_name if single_name == "#{name_first_word} Central"
-    # raise 'boom'
-
-    return single_name.gsub(name_first_word, name_first_word + ",") if SUBDIVIDED_CITIES.include?(name_first_word)
-
-    single_name
-  end
 
   def single_name_mapped_to_normalised_name(single_name)
     name_first_word = single_name.strip.split(" ").first
--- spec/models/constituency_spec.rb
+++ spec/models/constituency_spec.rb
@@ -8,25 +8,7 @@ RSpec.describe Constituency, type: :model do
   end
 
   specify { expect(subject).to respond_to(:name) }
-  specify { expect(subject).to respond_to(:name_mapped_to_ons_name) }
-
-  ons_examples = {
-    "Birmingham Ladywood" => "Birmingham, Ladywood",
-    "Liverpool Hallam" => "Liverpool, Hallam",
-    "Manchester Gorton" => "Manchester, Gorton",
-    "Manchester Central" => "Manchester Central",
-    "Sheffield Brightside and Hillsborough" => "Sheffield, Brightside and Hillsborough",
-    "Sheffield Central" => "Sheffield Central",
-    "Sheffield South East" => "Sheffield South East",
-    "Brighton Kemptown" => "Brighton, Kemptown",
-    "Plymouth Moorview" => "Plymouth, Moorview",
-    "Southampton Test" => "Southampton, Test",
-    "Ealing Southall" => "Ealing, Southall",
-    "Ealing North" => "Ealing North",
-    "Acton and Ealing Village" => "Acton and Ealing, Village",
-    "Lewisham Deptford" => "Lewisham, Deptford",
-    "Lewisham East" => "Lewisham East"
-  }
+  specify { expect(subject).to respond_to(:normalised_name) }
 
   normalised_examples = {
     "Birmingham Ladywood" => "Birmingham Ladywood",
@@ -53,14 +35,6 @@ RSpec.describe Constituency, type: :model do
     "South West Hertfordshire" => "Hertfordshire South West",
     "East Hampshire" => "Hampshire East",
   }
-
-  describe '#name_mapped_to_ons_name' do
-    ons_examples.each do |(our_name, ons_name)|
-      it "when given #{our_name}, returns #{ons_name}" do
-        expect(described_class.new(name: our_name).name_mapped_to_ons_name).to eql(ons_name)
-      end
-    end
-  end
 
   describe '#normalised_name' do
     normalised_examples.each do |(our_name, ons_name)|
