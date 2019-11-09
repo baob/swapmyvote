0761f196da93886d319baaf7edf52d1357d64bbf
baob <coder@onesandthrees.com>
2019-11-09 13:27:14 +0000

    normalising ONS names for compass points

--- app/models/ons_constituency.rb
+++ app/models/ons_constituency.rb
@@ -1,5 +1,32 @@
 class OnsConstituency < ApplicationRecord
+  COMPASS_PARTS = [
+    "North East",
+    "North West",
+    "South East",
+    "South West",
+    "North",
+    "East",
+    "West",
+    "South"
+  ]
+
   def normalised_name
-    name
+    name_split_by_and = name.match(/^(.*) and (.*)$/)
+    return single_name_mapped_to_normalised_name(name) if name_split_by_and.nil?
+    # puts "MULTIPLE NAME DETECTED #{name}"
+    name_split_by_and.captures.map{ |n| single_name_mapped_to_normalised_name(n) }.join("|")
+  end
+
+  private
+
+  def single_name_mapped_to_normalised_name(single_name)
+    compass_part = COMPASS_PARTS.detect{ |cp| single_name =~ Regexp.new(cp) }
+    # puts "COMPASS PART #{compass_part} detected"
+    unless compass_part.nil?
+      non_compass_part = single_name.gsub(compass_part, "").gsub("  ", " ").strip
+      return "#{non_compass_part} #{compass_part}"
+    end
+
+    single_name
   end
 end
--- spec/models/ons_constituency_spec.rb
+++ spec/models/ons_constituency_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe OnsConstituency, type: :model do
   specify { expect(subject).to respond_to(:name) }
   specify { expect(subject).to respond_to(:ons_id) }
   specify { expect(subject).to respond_to(:normalised_name) }
+
+  normalised_examples = {
+    "Acton and Ealing Village" => "Acton|Ealing Village",
+    "North Dorset" => "Dorset North",
+    "North East Glasgow" => "Glasgow North East",
+    "West Lancashire" => "Lancashire West",
+    "South Norfolk" => "Norfolk South",
+    "North West Hampshire" => "Hampshire North West",
+    "South East Sheffield" => "Sheffield South East",
+    "South West Hertfordshire" => "Hertfordshire South West",
+    "East Hampshire" => "Hampshire East"
+  }
+
+  describe '#normalised_name' do
+    normalised_examples.each do |(our_name, ons_name)|
+      it "when given #{our_name}, returns #{ons_name}" do
+        expect(described_class.new(name: our_name).normalised_name).to eql(ons_name)
+      end
+    end
+  end
+
+
 end
