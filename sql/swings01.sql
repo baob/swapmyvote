8400bfc08b0ebcddf2451f697cdc5720d3fcf8c5
baob <coder@onesandthrees.com>
2019-12-13 00:04:22 +0000

    wip-002 but wrong result

--- sql/swings01.sql
+++ sql/swings01.sql
@@ -27,13 +27,15 @@ where ((users.swap_id = swaps.id) or (users.id = swaps.chosen_user_id))
 and swaps.confirmed = true
 
 
-select user_id, swap_id, willing_party_id, preferred_party_id, swing_raw.ons_id, parties.id as party_id
+select swing_raw.ons_id, parties.id as party_id,
+  sum(CASE WHEN parties.id = willing_party_id THEN 1 else 0 end) as partyup,
+  sum(CASE WHEN parties.id = preferred_party_id THEN 1 else 0 end) as partydown
 from (
   select users.id as user_id, swaps.id as swap_id, willing_party_id, preferred_party_id, constituency_ons_id as ons_id
   from users, swaps
   where ((users.swap_id = swaps.id) or (users.id = swaps.chosen_user_id))
   and swaps.confirmed = true
-  limit 10
 ) as swing_raw
 left join parties  on swing_raw.willing_party_id = parties.id or swing_raw.preferred_party_id = parties.id
 left join ons_constituencies on swing_raw.ons_id = ons_constituencies.ons_id
+group by swing_raw.ons_id, party_id
