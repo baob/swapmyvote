0326b6ccf0ad8fabcd2b15d12e087139c2511fa8
baob <coder@onesandthrees.com>
2019-12-13 00:36:14 +0000

    new inner query

--- sql/swings01.sql
+++ sql/swings01.sql
@@ -27,15 +27,23 @@ where ((users.swap_id = swaps.id) or (users.id = swaps.chosen_user_id))
 and swaps.confirmed = true
 
 
-select swing_raw.ons_id, parties.id as party_id,
-  sum(CASE WHEN parties.id = willing_party_id THEN 1 else 0 end) as partyup,
-  sum(CASE WHEN parties.id = preferred_party_id THEN 1 else 0 end) as partydown
+select user_id, swap_id, willing_party_id, preferred_party_id, swing_raw.ons_id, parties.id as party_id,
+  (CASE WHEN parties.id = willing_party_id THEN 1 else 0 end) as partyup,
+  (CASE WHEN parties.id = preferred_party_id THEN 1 else 0 end) as partydown
 from (
   select users.id as user_id, swaps.id as swap_id, willing_party_id, preferred_party_id, constituency_ons_id as ons_id
   from users, swaps
   where ((users.swap_id = swaps.id) or (users.id = swaps.chosen_user_id))
   and swaps.confirmed = true
+  limit 10
 ) as swing_raw
 left join parties  on swing_raw.willing_party_id = parties.id or swing_raw.preferred_party_id = parties.id
 left join ons_constituencies on swing_raw.ons_id = ons_constituencies.ons_id
-group by swing_raw.ons_id, party_id
+
+select swaps.id as swap_id, usersa.id as usersa_id, usersb.id as usersb_id,
+  usersa.constituency_ons_id as onsa_id, usersb.constituency_ons_id as onsb_id,
+  usersa.willing_party_id as party_up_a_id, usersb.willing_party_id as party_up_b_id
+from swaps
+left join users as usersa on usersa.swap_id = swaps.id
+left join users as usersb on swaps.chosen_user_id = usersb.id
+  limit 10
