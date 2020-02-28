6cf926483448c99bcd87bafd7e4d28fadb1e9682
baob <coder@onesandthrees.com>
2020-02-28 22:24:21 +0000

    simple admin controller test

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 ENV["ADMIN_PASSWORD"] = "test_password"
 
 RSpec.describe AdminController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
   let(:mock_current_user) do
     create(:ready_to_swap_user1, id: 111, constituency: build(:ons_constituency))
   end
@@ -11,9 +13,12 @@ RSpec.describe AdminController, type: :controller do
     double(:mock_mailer_return, deliver_now: true)
   end
 
+  before do
+    allow(controller).to receive(:authenticate_or_request_with_http_basic).with(anything).and_return true
+  end
+
   it "sends email proofs" do
     allow(controller).to receive(:current_user).and_return(mock_current_user)
-    # allow(subject).to receive(:http_basic_authenticate_with)
 
     allow(controller).to receive(:authenticate_or_request_with_http_basic).with(anything).and_return true
 
@@ -34,5 +39,12 @@ RSpec.describe AdminController, type: :controller do
     get :send_email_proofs
 
     expect(flash[:errors]).not_to be_present
+  end
+
+  describe "GET #stats" do
+    it "returns http success" do
+      get :stats
+      expect(response).to have_http_status(:success)
+    end
   end
 end
