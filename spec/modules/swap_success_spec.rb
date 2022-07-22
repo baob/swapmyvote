d5798bb075914b5b8c1e5d9f78b054cf497cb1f9
baob <coder@onesandthrees.com>
2022-07-22 21:46:34 +0100

    spec for SwapSuccess.score_conf_or_not_value

--- spec/modules/swap_success_spec.rb
+++ spec/modules/swap_success_spec.rb
@@ -19,5 +19,44 @@ RSpec.describe SwapSuccess do
       expect([[key1, key2], [key2, key1]]).to include(subject.call(key2, key1))
     end
   end
+
+  describe ".score_conf_or_not_value" do
+
+    context "overall ratio of succesful/confirmed swaps, to unconfirmed swaps is 8:1" do
+      let(:ratio) { 8.0 }
+
+      context "and we have 24 confirmed swaps and 0 unconfirmed in this group" do
+        let(:value) do
+          v = Hash.new
+          v[true] = 24
+          v[false] = 0
+          v
+        end
+        specify { expect(described_class.score_conf_or_not_value(value,ratio)).to eq(2.0) }
+      end
+
+      context "and we have 0 confirmed swaps and 12 unconfirmed in this group" do
+        let(:value) do
+          v = Hash.new
+          v[true] = 0
+          v[false] = 12
+          v
+        end
+        specify { expect(described_class.score_conf_or_not_value(value,ratio)).to eq(0.0) }
+      end
+
+      context "and we have 24 confirmed swaps and 3 unconfirmed in this group (the average ratio)" do
+        let(:value) do
+          v = Hash.new
+          v[true] = 24
+          v[false] = 3
+          v
+        end
+        specify { expect(described_class.score_conf_or_not_value(value,ratio)).to eq(1.0) }
+      end
+
+    end
+
+  end
 end
 
