e297b5c5975f00b1d36a150451ecde147a405f90
baob <coder@onesandthrees.com>
2022-07-23 10:28:52 +0100

    massive refactor for readability

--- lib/modules/swap_success.rb
+++ lib/modules/swap_success.rb
@@ -1,9 +1,7 @@
 module SwapSuccess
-
   SMALL_GROUP_THRESHOLD = 8
 
   class << self
-
     def order_keys_for_uniqueness(k1, k2)
       k1.hash > k2.hash ? [k1, k2] : [k2, k1]
     end
@@ -17,12 +15,20 @@ module SwapSuccess
     end
 
     def keep_success_count(o, success_counts)
-      two_counts = [success_counts[o][false] || 0 , success_counts[o][true] || 0]; two_counts.sum > SMALL_GROUP_THRESHOLD
+      two_counts = [success_counts[o][false] || 0 , success_counts[o][true] || 0]
+
+      two_counts.sum > SMALL_GROUP_THRESHOLD
     end
 
     def swap_success_lookup
-      choosing  = User.left_joins(:outgoing_swap).where("swaps.id IS NOT ?", nil).where("users.constituency_ons_id LIKE '_%'").eager_load(outgoing_swap: :chosen_user)
-      expected_good_bad_ratio = choosing.where("swaps.confirmed = ?", true).count/Float(choosing.where("swaps.confirmed = ?", false).count)
+      choosing = User.left_joins(:outgoing_swap)
+        .where("swaps.id IS NOT ?", nil)
+        .where("users.constituency_ons_id LIKE '_%'")
+        .eager_load(outgoing_swap: :chosen_user)
+
+      total_confirmed = choosing.where(swaps: { confirmed: true }).count
+      total_unconfirmed = choosing.where(swaps: { confirmed: false }).count
+      expected_good_bad_ratio = total_confirmed / Float(total_unconfirmed)
 
       # puts "\nSCORING PRINCIPLE: scores (after =>) represent relative success at turning proposed swaps into confirmed swaps."
       # puts "2.0 means 100% of them, 0.0 means none of them."
@@ -32,12 +38,31 @@ module SwapSuccess
       # puts "E.g. fighting-2-winning indicates that voter's preferred party instead of getting a vote in marginal where they are fighting,"
       # puts "instead gets a vote in a constituency where they are winning"
 
-      threeway = choosing.map{ |c| o = c.outgoing_swap.chosen_user; [c.bucket_with(o.constituency_ons_id), o.bucket_with(c.constituency_ons_id), c.outgoing_swap.confirmed] }.tally
+      threeway = choosing.map do |chooser|
+        chosen = chooser.outgoing_swap.chosen_user
+        [chooser.bucket_with(chosen.constituency_ons_id), chosen.bucket_with(chooser.constituency_ons_id), chooser.outgoing_swap.confirmed]
+      end.tally
 
-      result = Hash.new{ |o,k| o[k] = Hash.new { |o,k| o[k] = 0 } }
-      success_counts = threeway.each_with_object(result) { |(k,v), r|  new_k = order_keys_for_uniqueness(k[0], k[1]) ;  new_sub_k = k[2] ; r[new_k][new_sub_k] = v  }
+      result = Hash.new{ |o, k| o[k] = Hash.new { |o, k| o[k] = 0 } }
 
-      lookup = success_counts.map{ |(pair, success_count)| [pair, score_conf_or_not_value(success_count, expected_good_bad_ratio)] }.to_h
+      # now get the true/false values into a single hash for each group
+      success_counts = threeway.each_with_object(result) do |(k, tally), r|
+        chooser_bucket = k[0]
+        chosen_bucket = k[1]
+        success_or_not = k[2]
+
+        # for figuring out if the swap is a success or not,
+        # the order of the keys (swap proposer/swap receiver) doesn't matter,
+        # both counts go into the same group
+        new_key = order_keys_for_uniqueness(chooser_bucket, chosen_bucket)
+
+        r[new_key][success_or_not] = tally
+      end
+
+      # now for each group, turn the pair of counts into a score, and return a hash of all of those
+      success_counts.map do |(pair, success_count)|
+        [pair, score_conf_or_not_value(success_count, expected_good_bad_ratio)]
+      end.to_h
     end
   end
 end
